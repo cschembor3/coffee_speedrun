e9d5d1e75523248fdab9544dc254e03666000f60
Connor Schembor <cschembor96@gmail.com>
2019-12-17 18:50:49 -0500

    Fix error when timer is stopped

--- lib/coffee_speedrun_web/commanders/page_commander.ex
+++ lib/coffee_speedrun_web/commanders/page_commander.ex
@@ -1,28 +1,32 @@
 defmodule CoffeeSpeedrunWeb.PageCommander do
-  use Drab.Commander, modules: [Drab.Query, Drab.Modal]
+  use Drab.Commander
+  use Drab.Commander, modules: [Drab.Query, Drab.Modal, Drab.Live, Drab.Core]
 
   defhandler start_stopwatch(socket, _sender) do
-    increment_timer(socket, ~T[00:00:00])
+    Process.register(spawn(fn -> increment_timer(socket, ~T[00:00:00]) end), :testing)
+  end
+
+  defhandler stop_stopwatch(socket, sender) do
+    IO.inspect(:testing)
+    send(:testing, :cancel_timer)
   end
 
   def increment_timer(socket, time) do
-      update_stopwatch(socket, Time.add(time, 1))
-      :timer.sleep(1 * 1000)
-      receive do
-        :cancel_processing ->
-          socket |> update(:text, set: "how do you do", on: "#stopwatch_div")
-        after 0 ->
+    update_stopwatch(socket, Time.add(time, 1))
+    :timer.sleep(1 * 1000)
+
+    receive do
+      :cancel_timer ->
+        socket |> update(:text, set: "how do you do", on: "#stopwatch_div")
+    after
+      0 ->
         increment_timer(socket, Time.add(time, 1))
-      end
+    end
   end
 
   def update_stopwatch(socket, new_time) do
     socket
-      |> update(:text, set: "#{format_time(new_time)}", on: "#stopwatch_div")
-  end
-
-  defhandler stop_stopwatch(socket, sender) do
-    send(Drab.pid(socket), :cancel_processing)
+    |> update(:text, set: "#{format_time(new_time)}", on: "#stopwatch_div")
   end
 
   def format_time(time) do
@@ -30,7 +34,7 @@ defmodule CoffeeSpeedrunWeb.PageCommander do
       time.hour,
       time.minute,
       time.second,
-      {0,0}
+      {0, 0}
     )
   end
 end
